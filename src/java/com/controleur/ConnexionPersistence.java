707f4c951582e2a9744d72e4aa143cc5d06025b8
esic <esic@esic-HP.home>
2018-06-15 10:00:46 +0200

    Premier commit (initialisation de repository)

--- src/java/com/controleur/ConnexionPersistence.java
+++ src/java/com/controleur/ConnexionPersistence.java
@@ -5,9 +5,11 @@
  */
 package com.controleur;
 
+import java.util.Collection;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -19,11 +21,34 @@ public class ConnexionPersistence {
     @PersistenceContext(unitName = "EmployesServletJPAPU")
     private EntityManager em;
     
-    //GET EMPLOYES (SELECT ALL EMPLOYES)
     //GET EMPLOYE PAR ID (PREPARED STMT)
     //MODIFIER EMPLOYES (PREPARED STATEMENT)
     //SUPPRIMER EMPLOYES (PREPARED STMT)
     //GET IDENTIFIANTS (SELECT ALL iDENTIFIANTS
+    
+    public Collection getEmployes(){
+        Query q = em.createQuery("SELECT e from Employes e");
+        return q.getResultList();
+    }
+    
+    public Collection getEmployesId(){
+        
+    }
+    
+    public Collection getIdentifiants(){
+        Query q = em.createQuery("SELECT i from Identifiants i");
+        return q.getResultList();
+    }
+    
+    public Collection modifierEmployes(){
+        Query q = em.createQuery("");
+        return q.getResultList();
+    }
+    
+    public Collection supprimerEmployes(){
+        Query q = em.createQuery("DELETE from Employes where id=?");
+        return q.getResultList();
+    }
 
     public void persist(Object object) {
         em.persist(object);
